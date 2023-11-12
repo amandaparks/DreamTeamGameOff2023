3d3396e3f848a183b0999c3d3b000b3b44da6251
Amanda Parks <amandaparks@hey.com>
2023-11-13 02:42:21 +1100

    AP - Got step fwd/bkd working! - Added Alannah's assets - Chucked in an old environment shader

--- Assets/Programming/DT_PlayerMovement.cs
+++ Assets/Programming/DT_PlayerMovement.cs
@@ -1,19 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class DT_PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
+
+    private GameManager _gameManager;
+
+    public float moveDuration;
+    public float jumpArcHeight;
     public float climbSpeed;
     private Vector3 _startPos;
     private Vector3 _targetPos;
-    
+    private bool isNoStonesBruh;
+    private bool isNoLaddersBruh;
+
+    private GameObject[] _steppingStones;
+    private GameObject[] _ladders;
+
+    private GameObject _nextStone;
+
+    private enum TargetDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+        Stop
+    }
+
+    private TargetDirection _targetDirection;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Find the Game Manager and assign it to this variable
+        _gameManager = GameManager.Instance;
+        
+        // Find all the stepping stones and ladders
+        _steppingStones = GameObject.FindGameObjectsWithTag("StepStone");
+        Debug.Log("Found "+_steppingStones.Length+" stones!");
+        _ladders = GameObject.FindGameObjectsWithTag("Ladder");
+        Debug.Log("Found "+_ladders.Length+" ladders!");
+
         _startPos = gameObject.transform.position;
+        
+        // If there are no stones or ladders, set bools to true
+        isNoStonesBruh = _steppingStones.Length == 0;
+        isNoLaddersBruh = _ladders.Length == 0;
     }
 
     // Update is called once per frame
@@ -24,18 +62,199 @@ public class DT_PlayerMovement : MonoBehaviour
 
     public void OnStepBkd()
     {
-        Debug.Log("Stepped backward");
+        Debug.Log("INPUT: StepBkd");
+        
+        // Check we can do it
+        if (!CanPerformAction("Step")) return;
+        
+        _targetDirection = TargetDirection.Left; 
+        Debug.Log("Starting Step Coroutine");
+        StartCoroutine(Step());
     }
+
     public void OnStepFwd()
     {
-        Debug.Log("Stepped forward");
+        Debug.Log("INPUT: StepFwd");
+        
+        // Check we can do it
+        if (!CanPerformAction("Step")) return;
+        
+        _targetDirection = TargetDirection.Right;
+        Debug.Log("Starting Step Coroutine");
+        StartCoroutine(Step());
+            
     }
+
+    private bool CanPerformAction(string actionType)
+    {
+        // Player can only move if game is not paused and they are already in Idle
+        if (_gameManager.gameState == GameManager.GameState.Paused)
+        {
+            Debug.Log($"{actionType} cancelled. Game is paused.");
+            return false;
+        }
+
+        if (_gameManager.playerState != GameManager.PlayerState.Idle)
+        {
+            Debug.Log($"{actionType} cancelled. Player not Idle.");
+            return false;
+        }
+
+        // Other checks...
+        switch (actionType)
+        {
+            case "Step":
+                if (isNoStonesBruh)
+                {
+                    Debug.Log($"{actionType} cancelled. No stones in the scene.");
+                    return false;
+                }
+                break;
+
+            case "Climb":
+                if (isNoLaddersBruh)
+                {
+                    Debug.Log($"{actionType} cancelled. No ladders in the scene.");
+                    return false;
+                }
+                break;
+            
+            default:
+                Debug.LogError($"Unknown action type: {actionType}");
+                return false;
+        }
+        
+        return true;
+    }
+
+    public void ResetToIdle()
+    {
+        _nextStone = null;
+        _targetDirection = TargetDirection.Stop;
+        _gameManager.playerState = GameManager.PlayerState.Idle;
+        Debug.Log("PARAMS RESET");
+    }
+
     public void OnClimb()
     {
         Debug.Log("Climbed");
     }
+
+    private IEnumerator Step()
+    {
+        // First, find next stone
+        _nextStone = FindNextStone();
+        
+        // If no stones found...
+        if (_nextStone == null)
+        {
+            Debug.Log("No more stones :(");
+        }
+        // Stone found...
+        else
+        {
+            Debug.Log("PlayerState.Stepping");
+            _gameManager.playerState = GameManager.PlayerState.Stepping;
+            
+            // Set start and end points of curve
+            var startPoint = transform.position;
+            var endPoint = _nextStone.transform.position;
+            // Start counting
+            float elapsedTime = 0f;
+
+            while (elapsedTime < moveDuration)
+            {
+                // Find where on curve player should be this frame
+                float t = elapsedTime / moveDuration;
+                Vector3 position = QuadraticBezier(startPoint, endPoint, jumpArcHeight, t);
+
+                // Set position
+                transform.position = position;
+
+                // Increment for next frame
+                elapsedTime += Time.deltaTime;
+
+                // Wait
+                yield return null;
+            }
+            
+            // Force player onto correct spot
+            transform.position = _nextStone.transform.position;
+            Debug.Log("Arrived at target!");
+            
+            ResetToIdle();
+            
+            yield return null;
+        }
+    }
+
+    private GameObject FindNextStone()
+    {
+        Debug.Log("Finding next stone...");
+        
+        // Reset _nextStone
+        _nextStone = null;
+        
+        Debug.Log("Checking each one...");
+        
+        // Compare the distance from player of each stone to the previous stone
+        // For the first stone, compare it to the biggest possible number (float.MaxValue)
+        
+        var closestDistance = float.MaxValue;
+        
+        foreach (GameObject stone in _steppingStones)
+        {
+            // Get distance b/w player and stone
+            var distanceToStone = Vector3.Distance(transform.position, stone.transform.position);
+
+            // Check if it's on the side we want
+            bool isTargetDirection = (_targetDirection == TargetDirection.Right && stone.transform.position.x > transform.position.x) ||
+                                      (_targetDirection == TargetDirection.Left && stone.transform.position.x < transform.position.x);
+
+            // If it's on the side we want and closer than any previous stones
+            if (isTargetDirection && distanceToStone < closestDistance)
+            {
+                //Set the new closest distance
+                closestDistance = distanceToStone;
+                
+                //Set the stone as the target
+                _nextStone = stone;
+            }
+        }
+        
+        // If no stones were found
+
+        if (_nextStone != null)
+        {
+            Debug.Log($"Closest stone is {_nextStone.name}");
+            // Return the updated stone
+            return _nextStone;
+        }
+
+        Debug.Log("No stones...");
+        return null;
+    }
+
     
-    
+// Quadratic Bezier curve calculation with added control point for height
+// Thanks to old mate chatGPT and https://www.gamedeveloper.com/business/how-to-work-with-bezier-curve-in-games-with-unity
+    private Vector3 QuadraticBezier(Vector3 p0, Vector3 p2, float height, float t)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+
+        Vector3 p = uu * p0; // (1-t)^2 * P0
+        p += 2 * u * t * (p2 + Vector3.up * height); // Use p0 + Vector3.up for height at start of curve
+        p += tt * p2; // t^2 * P2
+
+        return p;
+    }
+
+
+
+
+
     public void OnDuck()
     {
         Debug.Log("Ducked");
--- Assets/Programming/GameManager.cs
+++ Assets/Programming/GameManager.cs
@@ -4,10 +4,62 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Static variable so any script can access this script by using GameManager.Instance
+    public static GameManager Instance;
+    [HideInInspector] public GameState gameState;
+    // [HideInInspector] public CurrentScene currentScene;
+    [HideInInspector] public PlayerState playerState;
+
+    void Awake()
+    {
+        // We want one Game Manager to awake and persist for the rest of the game
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        // If the Game Manager awakens but another already exists, it should destroy itself
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public enum GameState
+    {
+        Playing,
+        Paused
+    }
+
+    public enum CurrentScene
+    {
+        WorldMap,
+        Level1,
+        Level2,
+        Level3,
+        Level4,
+        Level5,
+        Level6,
+        Summit
+    }
+
+    public enum PlayerState
+    {
+        Idle,
+        Talking,
+        Stepping,
+        Climbing,
+        Ducking,
+        Damaged,
+        Attacking,
+        Defending
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerState = PlayerState.Idle;
+        gameState = GameState.Playing;
     }
 
     // Update is called once per frame
