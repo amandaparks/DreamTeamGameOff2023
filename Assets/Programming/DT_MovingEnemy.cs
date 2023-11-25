49509e257cb80a9f78a9e576e638e4c01246a0cd
Amanda Parks <amandaparks@hey.com>
2023-11-26 01:43:38 +1100

    AP - Fixed rigid object bug by adding null check

--- Assets/Programming/DT_MovingEnemy.cs
+++ Assets/Programming/DT_MovingEnemy.cs
@@ -3,50 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = System.Random;
 
 public class DT_MovingEnemy : MonoBehaviour
 {
-    [Header("== Enemy Behaviour ==")]
-    [SerializeField] private Collider damageCollider;
+    [Header("== Enemy Behaviour ==")] [SerializeField]
+    private Collider damageCollider;
+
     [SerializeField] private MovementType moveType;
+
     private enum MovementType
     {
         fallStraightDown,
         screenRightToLeft,
         shootTowardPlayer
     }
+
     [HideInInspector] public float moveSpeed;
     [HideInInspector] public float maxLifeSpan = 5f;
-    
-    
+
+
     [Header("== Player Interaction ==")]
     //[SerializeField] private bool playerCanDefend;
-    [SerializeField] private bool crouchIsSafe;
-    
-    [Header("== Damage Player Sound / Effect ==")]
-    [SerializeField] private ParticleSystem damageParticles;
+    [SerializeField]
+    private bool crouchIsSafe;
+
+    [Header("== Damage Player Sound / Effect ==")] [SerializeField]
+    private ParticleSystem damageParticles;
+
     [SerializeField] private AudioClip damageSound;
     [HideInInspector] public AudioSource damageAudioSource;
-    
-    [Header("== Always-On Sound / Effects ==")]
-    [SerializeField] private AudioClip alwaysOnSound;
+
+    [Header("== Always-On Sound / Effects ==")] [SerializeField]
+    private AudioClip alwaysOnSound;
+
     [SerializeField] private ParticleSystem alwaysOnParticles;
     [HideInInspector] public AudioSource alwaysOnAudioSource;
 
-    [Header("== Collide Environment Settings ==")] 
-    [SerializeField] private Animation collisionAnimation;
+    [Header("== Collide Environment Settings ==")] [SerializeField]
+    private Animation collisionAnimation;
+
     [SerializeField] private ParticleSystem collisionParticles;
     [SerializeField] private AudioClip collisionSound;
     [HideInInspector] public AudioSource collisionAudioSource;
     [SerializeField] private string environmentTag;
     [SerializeField] private bool dropOnCollide;
 
-    [Header("== Defendable Enemy Settings ==")]
-    [SerializeField] private bool playerCanDefend;
+    [Header("== Defendable Enemy Settings ==")] [SerializeField]
+    private bool playerCanDefend;
+
     [SerializeField] private ParticleSystem disabledParticles;
     [SerializeField] private AudioClip disabledSound;
     [HideInInspector] public AudioSource disabledAudioSource;
-    
+
     private GameObject _player;
     private bool _isEnemyActive;
     private bool _canCoroutineRun;
@@ -55,16 +64,18 @@ public class DT_MovingEnemy : MonoBehaviour
     private GameManager.PlayerState _oldPlayerState = GameManager.PlayerState.Idle; // Initialise as idle
     private GameManager.GameState _previousGameState = GameManager.GameState.Playing; // Initialise as playing
     public float maxMoveDistance;
-    
+
     // For use in calculating targets
     // Serialized for debugging
     [SerializeField] private Vector3 _startPoint;
     [SerializeField] private Vector3 _endPoint;
     [SerializeField] private Vector3 _playerPosition;
+
     [SerializeField] private Vector3 _enemyStartPosition;
-   // [SerializeField] private float _distance;
-   [SerializeField] private float _distanceFromStart;
-    
+
+    // [SerializeField] private float _distance;
+    [SerializeField] private float _distanceFromStart;
+
     /* NOTE:
   *  Moving enemies are:
   *    - Falling rocks
@@ -89,33 +100,33 @@ public class DT_MovingEnemy : MonoBehaviour
             _canCoroutineRun = false;
         }
     }
-    
-    
-    
+
+
+
     void OnEnable()
     {
         // Find the player and scripts
         _player = GameObject.FindWithTag("Player");
         _playerDamage = _player.GetComponent<DT_PlayerDamage>();
-        
+
         // Instantiate audio sources
         alwaysOnAudioSource = gameObject.AddComponent<AudioSource>();
         damageAudioSource = gameObject.AddComponent<AudioSource>();
         collisionAudioSource = gameObject.AddComponent<AudioSource>();
         disabledAudioSource = gameObject.AddComponent<AudioSource>();
-        
+
         // Assign targets depending on move type
         AssignTargets(moveType);
 
         // Turn on
         EnemyToggle(true);
-        
+
         // Start moving
         _canCoroutineRun = true;
         StartCoroutine(Move());
 
         // Start self-destruct countdown
-        Destroy(gameObject,maxLifeSpan);
+        Destroy(gameObject, maxLifeSpan);
     }
 
     private void AssignTargets(MovementType type)
@@ -133,13 +144,15 @@ public class DT_MovingEnemy : MonoBehaviour
         {
             case MovementType.fallStraightDown:
                 // End point is further down on the Y axis
-                _endPoint = new Vector3(_enemyStartPosition.x, _enemyStartPosition.y - maxMoveDistance, _enemyStartPosition.z);
+                _endPoint = new Vector3(_enemyStartPosition.x, _enemyStartPosition.y - maxMoveDistance,
+                    _enemyStartPosition.z);
                 // Distance
-               // _distance = Vector3.Distance(_startPoint, _endPoint);
+                // _distance = Vector3.Distance(_startPoint, _endPoint);
                 break;
             case MovementType.screenRightToLeft:
                 // End point is further left on the X axis
-                _endPoint = new Vector3(_enemyStartPosition.x - maxMoveDistance, _enemyStartPosition.y, _enemyStartPosition.z);
+                _endPoint = new Vector3(_enemyStartPosition.x - maxMoveDistance, _enemyStartPosition.y,
+                    _enemyStartPosition.z);
                 // Distance
                 //_distance = Vector3.Distance(_startPoint, _endPoint);
                 break;
@@ -168,6 +181,7 @@ public class DT_MovingEnemy : MonoBehaviour
                     EnemyToggle(false);
                     break;
             }
+
             _previousGameState = GameManager.CurrentGameState;
         }
 
@@ -179,7 +193,7 @@ public class DT_MovingEnemy : MonoBehaviour
             {
                 EnemyToggle(true);
             }
-            
+
             switch (GameManager.CurrentPlayerState)
             {
                 case GameManager.PlayerState.Defending:
@@ -187,17 +201,18 @@ public class DT_MovingEnemy : MonoBehaviour
                     {
                         OnPlayerDefend();
                     }
+
                     break;
                 case GameManager.PlayerState.BardMode:
                     EnemyToggle(false);
                     break;
             }
-            
+
             _oldPlayerState = GameManager.CurrentPlayerState;
-            
+
         }
     }
-    
+
 
     private IEnumerator Move()
     {
@@ -209,22 +224,22 @@ public class DT_MovingEnemy : MonoBehaviour
         // Start counting
         float elapsedTime = 0f;
 
-        while (elapsedTime < (5f/moveSpeed))
+        while (elapsedTime < (5f / moveSpeed))
         {
             if (!_canCoroutineRun) yield return null; // skip the rest of the loop
 
             // Calculate distance travelled (Math.Abs = positive value)
             _distanceFromStart = Math.Abs(Vector3.Distance(_startPoint, transform.position));
-            
+
             // If current distance is equal to or greater than max distance, disable and drop
             if (_distanceFromStart >= maxMoveDistance)
             {
                 StartCoroutine(DisableWaitDrop(0f));
                 yield break;
             }
-            
+
             // Move enemy in assigned direction
-            transform.position = Vector3.Lerp(_startPoint, _endPoint, elapsedTime / (5f/moveSpeed));
+            transform.position = Vector3.Lerp(_startPoint, _endPoint, elapsedTime / (5f / moveSpeed));
 
             // If it's supposed to be shooting toward the player, make object face the player
             if (moveType == MovementType.shootTowardPlayer)
@@ -238,7 +253,7 @@ public class DT_MovingEnemy : MonoBehaviour
             // Wait for next frame
             yield return null;
         }
-        
+
         yield return null;
     }
 
@@ -250,15 +265,19 @@ public class DT_MovingEnemy : MonoBehaviour
             {
                 alwaysOnParticles.Play();
             }
+
             if (alwaysOnSound == null) return;
             alwaysOnAudioSource.clip = alwaysOnSound;
             alwaysOnAudioSource.Play();
-            
+
         }
         else //false
         {
             if (alwaysOnParticles != null)
-            {alwaysOnParticles.Stop();}
+            {
+                alwaysOnParticles.Stop();
+            }
+
             if (alwaysOnSound == null) return;
             alwaysOnAudioSource.Stop();
         }
@@ -275,15 +294,16 @@ public class DT_MovingEnemy : MonoBehaviour
             if (GameManager.CurrentPlayerState == GameManager.PlayerState.Damaged) return;
             if (GameManager.CurrentPlayerState == GameManager.PlayerState.BardMode) return;
             if (crouchIsSafe && GameManager.CurrentPlayerState == GameManager.PlayerState.Crouching) return;
-            
+
             // Cause damage to player (OnPlayerCrouch handles crouching)
             _playerDamage.DamagePlayer();
-            
+
             // Do effects and sound
             if (damageParticles != null)
             {
                 damageParticles.Play();
             }
+
             if (damageSound == null) return;
             damageAudioSource.clip = damageSound;
             damageAudioSource.Play();
@@ -297,15 +317,18 @@ public class DT_MovingEnemy : MonoBehaviour
             {
                 collisionParticles.Play();
             }
+
             if (collisionSound != null)
             {
                 collisionAudioSource.clip = damageSound;
                 collisionAudioSource.Play();
             }
+
             if (collisionAnimation != null)
             {
                 collisionAnimation.Play();
             }
+
             if (dropOnCollide)
             {
                 StartCoroutine(DisableWaitDrop(0f));
@@ -323,11 +346,12 @@ public class DT_MovingEnemy : MonoBehaviour
     {
         // If not defendable enemy, ignore
         if (!playerCanDefend) return;
-        
+
         if (disabledParticles != null)
         {
             disabledParticles.Play();
         }
+
         if (disabledSound != null)
         {
             disabledAudioSource.Play();
@@ -341,15 +365,21 @@ public class DT_MovingEnemy : MonoBehaviour
         // Turn off collider
         damageCollider.enabled = false;
         damageCollider.isTrigger = false;
-        
+
         // Stop running coroutine
         _canCoroutineRun = false;
-        
+
         // Allow Unity to catch up
         yield return new WaitForSeconds(seconds);
-        
-        // Turn off kinematic so it falls with gravity
-        gameObject.GetComponent<Rigidbody>().isKinematic = false;
-    }
 
+        // Turn off kinematic so it falls with gravity
+
+        if (gameObject.GetComponent<Rigidbody>() != null)
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+
+        }
+    }
 }
+
+
